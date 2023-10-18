f5cfd692643dd9df2ed0291ef3afaf71d5157af5
Nirob Hasan <nirob427417@gmail.com>
2023-10-18 10:33:46 +0600

    service details page

--- src/app/(public)/services/[id]/page.tsx
+++ src/app/(public)/services/[id]/page.tsx
@@ -1,17 +1,124 @@
 "use client";
+import BookingModal from "@/components/ui/BookingModal";
+import { useAddBookingMutation } from "@/redux/api/bookingApi";
 import { useServiceQuery } from "@/redux/api/serviceApi";
+import { Button, Card, Image, Spin, Tag, Typography, message } from "antd";
+import { useState } from "react";
 
-const ServicePage = ({ params }: any) => {
+const { Title, Paragraph } = Typography;
+
+const ServiceDetailsPage = ({ params }: any) => {
   const { id } = params;
   const { data, isLoading } = useServiceQuery(id);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [bookingDate, setBookingDate] = useState(null);
+
+  const [addBooking] = useAddBookingMutation();
+
+  const handleBooking = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = async () => {
+    try {
+      if (!bookingDate) {
+        message.error("Please select a booking date.");
+        return; // Return early if no booking date is selected
+      }
+
+      const bookingData = {
+        bookingServices: bookingDate
+          ? [
+              {
+                serviceId: service.id,
+                bookingDate: bookingDate,
+              },
+            ]
+          : [],
+      };
+
+      const res = await addBooking(bookingData).unwrap();
+      console.log(res);
+      if (res.id) {
+        message.success("Service booked successfully.");
+        setIsModalVisible(false);
+      }
+    } catch (err: any) {
+      message.error(err.message);
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleDateChange = (date: any) => {
+    setBookingDate(date); // No need for toDate() here
+  };
+
+  if (isLoading) {
+    return <Spin spinning={true}>Loading...</Spin>;
+  }
+
+  const service = data;
+  console.log(service);
+
+  if (!service) {
+    return null; // Service not found
+  }
+
   return (
-    <div>
-      <div className="w-[500px] h-[400px]">
-        <img src={data?.image} alt="" />
-      </div>
+    <div
+      style={{
+        marginTop: "30px",
+        padding: "20px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Spin spinning={isLoading}>
+        {service && (
+          <Card>
+            <div className="flex">
+              <div className="w-1/2">
+                <Image
+                  src={service.image}
+                  alt="Service Image"
+                  style={{ maxWidth: "500px", maxHeight: "500px" }}
+                />
+              </div>
+              <div className="m-10">
+                <Title level={3}>{service?.title}</Title>
+                <Paragraph>Price: ${service?.price}</Paragraph>
+                <Paragraph>
+                  Availability:{" "}
+                  <Tag color={service?.availability ? "success" : "error"}>
+                    {service?.availability ? "Available" : "Not Available"}
+                  </Tag>
+                </Paragraph>
+                <Paragraph>
+                  Category:{" "}
+                  {service?.category ? service?.category?.title : "N/A"}
+                </Paragraph>
+                <Paragraph>{service?.description}</Paragraph>
+                <Button type="primary" onClick={handleBooking}>
+                  Booking
+                </Button>
+                <BookingModal
+                  title="Booking Model"
+                  visible={isModalVisible}
+                  onOk={handleOk}
+                  onCancel={handleCancel}
+                  setBookingDate={handleDateChange}
+                />
+              </div>
+            </div>
+          </Card>
+        )}
+      </Spin>
     </div>
   );
 };
 
-export default ServicePage;
+export default ServiceDetailsPage;
